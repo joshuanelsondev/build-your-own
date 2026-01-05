51257bb8169ac592b771b3766d92cf44696704de
joshuanelsondev <joshuanelsondev@gmail.com>
2026-01-04 20:01:23 -0500

    Initial Commit, add tool-tutorial directory

---
+++ tool-tutorial/tool/bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const arg = require('arg');
+
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+  });
+
+  if (args['--start']) {
+    console.log('starting the app');
+  }
+} catch (e) {
+  console.log(e.message);
+}
+
+
+
